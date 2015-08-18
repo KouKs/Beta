e52de8813d4d5a365ec388cf5ae510d578864ad4
bausano <gtasampserver@seznam.cz>
2015-08-18 23:25:59 +0200

    Pro pavlíka :*

--- game/src/game/menu/MainMenu.java
+++ game/src/game/menu/MainMenu.java
@@ -6,6 +6,8 @@ package game.menu;
 
 import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Color;
 
 /**
  *
@@ -17,6 +19,10 @@ public class MainMenu {
         JFrame frame = new JFrame("MainMenu");
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         frame.setSize( new Dimension( 1024 , 768 ) );
+        frame.getContentPane().setBackground(Color.WHITE);
+        JLabel label = new JLabel("My label");
+        label.setText("<html><style>ul { list-style-type: none; }</style><ul> <li><b>Menu</b> <li>Pavlik</li> <li>ma</li> <li>za usima</li></ul></html>");
+        frame.add(label);
         frame.setVisible( true );
     }
 }
