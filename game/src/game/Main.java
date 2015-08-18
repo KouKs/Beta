11f3bf73b93b89a3b0f80eb66a83b7fa57149e94
KouKs <kouks.koch@gmail.com>
2015-08-18 12:26:20 +0200

    Project built with NB
    NetBeans generated document tree

---
+++ game/src/game/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2015 Michael Bausano & Pavel Koch
+ * This is a beta version of some random game
+ */
+package game;
+
+import java.awt.image.BufferStrategy;
+
+/**
+ *
+ * @author Pavel
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
