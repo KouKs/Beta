1026b5388b951952c33bff35fc367057821afb00
KouKs <kouks.koch@gmail.com>
2015-08-19 11:52:15 +0200

    Odstranění xml, přidání nefunkčního BufferStrategy

--- game/src/game/Main.java
+++ game/src/game/Main.java
@@ -23,6 +23,7 @@ public class Main {
     public static void main(String[] args) {
         // loading screens...
         
+        
         MainMenu m = new MainMenu( );
         m.load( );
     }
