89ef3cb8f048dcb1bdd6f1519f7fd9bcfc699449
KouKs <kouks.koch@gmail.com>
2015-08-19 11:53:29 +0200

    Odstranění xml, přidání nefunkčního BufferStrategy

--- game/src/game/menu/MainMenu.java
+++ game/src/game/menu/MainMenu.java
@@ -5,35 +5,63 @@
 package game.menu;
 
 import java.awt.Dimension;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JFrame;;
 import java.awt.Color;
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.image.BufferStrategy;
+import java.awt.Canvas;
 
 /**
  *
  * @author Pavel
  */
-public class MainMenu {
+public class MainMenu extends Canvas {
+    private final BufferStrategy strategy;
     
-    public void load( ) throws FileNotFoundException {
-        JFrame frame = new JFrame("MainMenu");
+    // performance settings. 
+    static {
+        System.setProperty("sun.java2d.transaccel", "True");
+        // System.setProperty("sun.java2d.trace", "timestamp,log,count");
+        // System.setProperty("sun.java2d.opengl", "True");
+        System.setProperty("sun.java2d.d3d", "True");
+        System.setProperty("sun.java2d.ddforcevram", "True");
+    }
+    
+    public MainMenu( ) {
+        this.createBufferStrategy(3);
+        this.strategy = this.getBufferStrategy();        
+    }
+    
+    public void load( ) {
+        JFrame frame = new JFrame("Main Menu");
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         // TODO: load default sizes from config
         frame.setSize( new Dimension( 800 , 600 ) );
-        frame.getContentPane().setBackground(Color.WHITE);
         
-        // Getting the menu XML file and printing it out
-        // TODO: path, layout?? 
-        String content = new Scanner( new File(
-                "X:/UwAmp/www/roguelike/Beta/game/build/classes/game/menu.xml"
-        )).useDelimiter("\\Z").next();
-        JLabel menuHTML = new JLabel("menuHTML");
-        menuHTML.setText( content );
-        frame.add( menuHTML );        
-        
-        frame.setVisible( true );
+        // the back buffer graphics object
+        Graphics2D bkG = (Graphics2D) strategy.getDrawGraphics(); 
+
+        // clear the backbuffer, this could be substituted for a background
+        // image or a tiled background.
+        bkG.setPaint(Color.WHITE);
+        bkG.fillRect(0, 0, getWidth(), getHeight());
+
+        // TODO: Draw your game world, or scene or anything else here.
+
+        // Rectangle2D is a shape subclass, and the graphics object can render
+        // it, makes things a little easier.
+        bkG.setColor(Color.green.darker());
+        // bkG.fill( new Shape( ) {} );
+
+        // properly dispose of the backbuffer graphics object. Release resources
+        // and cleanup.
+        bkG.dispose();
+
+        // flip/draw the backbuffer to the canvas component.
+        strategy.show();
+
+        // synchronize with the display refresh rate.
+        Toolkit.getDefaultToolkit().sync();
     }
 }
